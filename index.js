4c5220af72afbf309287c70fea30e074afcd8708
cicomsa <ioanacomsa85@gmail.com>
2018-10-14 01:30:13 +0100

    animation - div-3 reveal div-2 only when landing the first time on the page or after refresh

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
 const button = document.querySelector('.toggle');
 const toggleWrap = document.querySelector('.wrap-toggle');
 const wrapper = document.querySelector('.mainDiv');
+const div3 = document.querySelector('.div-3');
 
 button.addEventListener('mouseover', () => {
   wrapper.setAttribute('data-status', 'open');
-});
-
-wrapper.addEventListener('mouseover', () => {
-  wrapper.setAttribute('data-status', 'open');
-});
-
-wrapper.addEventListener('mouseout', () => {
-  wrapper.setAttribute('data-status', 'close');
+  div3.setAttribute('data-status', 'away');
+  if ((div3.style.right = '51%')) {
+    window.setTimeout(() => (div3.style.display = 'none'), 2000);
+  }
 });
 
 button.addEventListener('mouseout', () => {
   wrapper.setAttribute('data-status', 'close');
+  div3.setAttribute('data-status', 'in');
+  window.setTimeout(() => (div3.style.display = 'none'), 2000);
+});
+
+wrapper.addEventListener('mouseover', () => {
+  wrapper.setAttribute('data-status', 'open');
+  div3.setAttribute('data-status', 'away');
+  div3.style.display = 'none';
+});
+
+wrapper.addEventListener('mouseout', () => {
+  wrapper.setAttribute('data-status', 'close');
+  div3.setAttribute('data-status', 'in');
 });
